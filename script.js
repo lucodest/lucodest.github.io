fbbfdfc40a7e240a0ec8682f6c1182d285b36308
lucodest <42046805+lucodest@users.noreply.github.com>
2020-08-06 20:08:45 -0300

    small improvments

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ window.onload = function(){
 	
 	var md = new XMLHttpRequest();
 	md.onload = function(){
-		let fp = "<i>" + md.responseText.replace(/\n/gi,"<br>") + "</i>";
+		let fp = '<i>"' + md.responseText.replace(/\r\n/g,"<br>") + '"</i>';
 		msg.innerHTML = fp;
 	}
 	md.open("GET", "https://cors-anywhere.herokuapp.com/https://pastebin.com/raw/Bb4uhaDh");
 	md.send();
 }
+
